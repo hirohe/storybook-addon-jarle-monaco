eced7f34bc93d0cb722174f5d7f5e47102b4230c
hirohe <hesreg@hotmail.com>
2022-08-29 12:56:02 +0800

    feat: add STORYBOOK_ADDON_MONACO_PATH env to override monacoPath

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,10 @@
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "../src/preset.js"
-  ],
-  "framework": "@storybook/react"
+  stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '../src/preset.js'],
+  framework: '@storybook/react',
+  env: config => ({
+    ...config,
+    // example to set a custom monaco cdn path
+    STORYBOOK_ADDON_MONACO_PATH: 'https://cdn.staticfile.org/monaco-editor/0.33.0/min/vs',
+  }),
 }
--- src/register.js
+++ src/register.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { addons, types } from '@storybook/addons'
 import { AddonPanel } from '@storybook/components'
+import { loader } from '@monaco-editor/react'
 import LiveEditor from './components/LiveEditor'
+
+const monacoPath = process.env.STORYBOOK_ADDON_MONACO_PATH || 'https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0/min/vs'
+loader.config({ paths: { vs: monacoPath } })
 
 addons.register('storybook/react-live-edit', (api) => {
   addons.addPanel('storybook/react-live-edit/panel', {
